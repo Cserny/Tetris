7c2f73564e17bf5f70f03d0fae617b2135555498
Cserny <e.cseresnyes@yahoo.de>
2015-08-13 21:01:41 +0200

    Changes Tile.Status to a bool and Tile.Rotate() to a simplified version

--- Tetris/Tile.cs
+++ Tetris/Tile.cs
@@ -11,7 +11,7 @@ namespace Tetris
     {
         public int[]  X, Y;
         public int Sort;
-        public int Status; //For rotating purposes
+        public bool Status; //For rotating purposes
 
         public Tile(int sort)
         {
@@ -51,7 +51,7 @@ namespace Tetris
                     break;
             }
             Sort = sort;
-            Status = 0;
+            Status = true;
         }
         public void Move(int x, int y)
         {
@@ -63,214 +63,56 @@ namespace Tetris
         }
         public void Rotate()
         {
+            int sign = (Status ? 1 : -1);
             switch (Sort)
             {
                 case 1: //I-tile
-                    switch (Status % 4)
-                    {
-                        case 0:
-                            X[0] += 2;
-                            X[1] += 1;
-                            X[3] -= 1;
-                            Y[0] -= 1;
-                            Y[2] += 1;
-                            Y[3] += 2;
-                            break;
-                        case 1:
-                            X[0] += 1;
-                            X[2] -= 1;
-                            X[3] -= 2;
-                            Y[0] += 2;
-                            Y[1] += 1;
-                            Y[3] -= 1;
-                            break;
-                        case 2:
-                            X[0] -= 2;
-                            X[1] -= 1;
-                            X[3] += 1;
-                            Y[0] += 1;
-                            Y[2] -= 1;
-                            Y[3] -= 2;
-                            break;
-                        case 3:
-                            X[0] -= 1;
-                            X[2] += 1;
-                            X[3] += 2;
-                            Y[0] -= 2;
-                            Y[1] -= 1;
-                            Y[3] += 1;
-                            break;
-                    }
-                    break;
+                    X[0] += 2 * sign;
+                    X[1] += 1 * sign;
+                    X[3] -= 1 * sign;
+                    Y[0] -= 1 * sign;
+                    Y[2] += 1 * sign;
+                    Y[3] += 2 * sign;
+                    break;  
                 case 2: //J-tile
-                    switch (Status % 4)
-                    {
-                        case 0:
-                            X[0] += 1;
-                            X[2] -= 1;
-                            X[3] -= 2;
-                            Y[1] -= 1;
-                            Y[3] += 1;
-                            break;
-                        case 1:
-                            X[1] += 1;
-                            X[3] -= 1;
-                            Y[0] += 1;
-                            Y[2] -= 1;
-                            Y[3] -= 2;
-                            break;
-                        case 2:
-                            X[0] -= 1;
-                            X[2] += 1;
-                            X[3] += 2;
-                            Y[1] += 1;
-                            Y[3] -= 1;
-                            break;
-                        case 3:
-                            X[1] -= 1;
-                            X[3] += 1;
-                            Y[0] -= 1;
-                            Y[2] += 1;
-                            Y[3] += 2;
-                            break;
-                    }
+                    X[0] += 1 * sign;
+                    X[2] -= 1 * sign;
+                    X[3] -= 2 * sign;
+                    Y[1] -= 1 * sign;
+                    Y[3] += 1 * sign;
                     break;
                 case 3: //L-tile
-                    switch (Status % 4)
-                    {
-                        case 0:
-                            X[1] += 1;
-                            X[3] -= 1;
-                            Y[0] += 1;
-                            Y[1] -= 2;
-                            Y[2] -= 1;
-                            break;
-                        case 1:
-                            X[0] -= 1;
-                            X[1] += 2;
-                            X[2] += 1;
-                            Y[1] += 1;
-                            Y[3] -= 1;
-                            break;
-                        case 2:
-                            X[1] -= 1;
-                            X[3] += 1;
-                            Y[0] -= 1;
-                            Y[1] += 2;
-                            Y[2] += 1;
-                            break;
-                        case 3:
-                            X[0] += 1;
-                            X[1] -= 2;
-                            X[2] -= 1;
-                            Y[1] -= 1;
-                            Y[3] += 1;
-                            break;
-                    }
+                    X[1] += 1 * sign;
+                    X[3] -= 1 * sign;
+                    Y[0] += 1 * sign;
+                    Y[1] -= 2 * sign;
+                    Y[2] -= 1 * sign;
                     break;
                 case 4: //O-tile - doesn't need to be rotated
                     break;
                 case 5: //S-tile
-                    switch (Status % 4)
-                    {
-                        case 0:
-                            X[0] += 1;
-                            X[2] += 1;
-                            Y[1] += 1;
-                            Y[2] -= 2;
-                            Y[3] -= 1;
-                            break;
-                        case 1:
-                            X[1] -= 1;
-                            X[2] += 2;
-                            X[3] += 1;
-                            Y[0] += 1;
-                            Y[2] += 1;
-                            break;
-                        case 2:
-                            X[0] -= 1;
-                            X[2] -= 1;
-                            Y[1] -= 1;
-                            Y[2] += 2;
-                            Y[3] += 1;
-                            break;
-                        case 3:
-                            X[1] += 1;
-                            X[2] -= 2;
-                            X[3] -= 1;
-                            Y[0] -= 1;
-                            Y[2] -= 1;
-                            break;
-                    }
+                    X[0] += 1 * sign;
+                    X[2] += 1 * sign;
+                    Y[1] += 1 * sign;
+                    Y[2] -= 2 * sign;
+                    Y[3] -= 1 * sign;
                     break;
                 case 6: //T-tile
-                    switch (Status % 4)
-                    {
-                        case 0:
-                            X[0] += 1;
-                            X[3] -= 1;
-                            X[1] += 1;
-                            Y[1] -= 2;
-                            Y[2] -= 1;
-                            break;
-                        case 1:
-                            X[1] += 2;
-                            X[2] += 1;
-                            Y[0] += 1;
-                            Y[1] += 1;
-                            Y[3] -= 1;
-                            break;
-                        case 2:
-                            X[0] -= 1;
-                            X[3] += 1;
-                            X[1] -= 1;
-                            Y[1] += 2;
-                            Y[2] += 1;
-                            break;
-                        case 3:
-                            X[1] -= 2;
-                            X[2] -= 1;
-                            Y[0] -= 1;
-                            Y[1] -= 1;
-                            Y[3] += 1;
-                            break;
-                    }
+                    X[0] += 1 * sign;
+                    X[3] -= 1 * sign;
+                    X[1] += 1 * sign;
+                    Y[1] -= 2 * sign;
+                    Y[2] -= 1 * sign;
                     break;
                 case 7: //Z-tile
-                    switch (Status % 4)
-                    {
-                        case 0:
-                            X[0] += 2;
-                            X[1] += 1;
-                            X[3] -= 1;
-                            Y[0] -= 1;
-                            Y[2] -= 1;
-                            break;
-                        case 1:
-                            X[0] += 1;
-                            X[2] += 1;
-                            Y[0] += 2;
-                            Y[1] += 1;
-                            Y[3] -= 1;
-                            break;
-                        case 2:
-                            X[0] -= 2;
-                            X[1] -= 1;
-                            X[3] += 1;
-                            Y[0] += 1;
-                            Y[2] += 1;
-                            break;
-                        case 3:
-                            X[0] -= 1;
-                            X[2] -= 1;
-                            Y[0] -= 2;
-                            Y[1] -= 1;
-                            Y[3] += 1;
-                            break;
-                    }
+                    X[0] += 2 * sign;
+                    X[1] += 1 * sign;
+                    X[3] -= 1 * sign;
+                    Y[0] -= 1 * sign;
+                    Y[2] -= 1 * sign;
                     break;
             }
-            Status++;
+            Status = !Status;
         }
     }
 }
